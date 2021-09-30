0f2c866e41a62bcb0049192f71298a367f719b5b
toonvanderlely <toon.vanderlely@gmail.com>
2021-09-30 13:51:07 +0200

    footer toegevoegd

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -24,6 +24,41 @@
  } 
 //--> 
 </script>
-<footer>
-	© {{ __('misc.copyright') }}
+  <!-- Site footer -->
+  <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>Over ons</h6>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laborum deleniti, quae, maxime ratione rerum dolore quas doloremque voluptatibus sint quo magnam saepe, officiis minus laudantium praesentium ipsum voluptates amet?</p>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <h6>Social media</h6>
+            <ul class="footer-links">
+              <li><a href="#">instagram</a></li>
+              <li><a href="#">facebook</a></li>
+              <li><a href="#">linkedin</a></li>
+              <li><a href="#">pinterest</a></li>
+              <li><a href="#">twitter</a></li>
+            </ul>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="#">Over ons</a></li>
+              <li><a href="#">Contact Ons</a></li>
+              <li><a href="#">Home pagina</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">	© {{ __('misc.copyright') }} </p>.
+            </p>
+          </div>
+        </div>
+      </div>
 </footer>
